3693d5f0656bfe78f0bd97c7bc07bf6ad7b42dda
wkh <wukehong@sina.com>
2019-06-14 10:39:41 +0800

    封装        输出日志 go-fwrite/SetOutput

--- logger.go
+++ logger.go
@@ -11,12 +11,28 @@ var (
 	outVal atomic.Value
 )
 
-func init() {
-	outVal.Store(os.Stdout)
+type logger struct {
+	io.Writer
 }
 
-func SetOutput(out io.Writer) {
-	outVal.Store(out)
+func (w logger) Write(p []byte) (n int, err error) {
+	if len(p) > 0 {
+		return w.Write(p)
+	}
+	return 0, nil
+}
+
+func init() {
+	SetOutput(os.Stdout)
+}
+
+func SetOutput(output io.Writer) {
+	if out, ok := output.(*logger); ok {
+		outVal.Store(out)
+	} else {
+		outVal.Store(&logger{output})
+	}
+
 }
 
 func output() io.Writer {
